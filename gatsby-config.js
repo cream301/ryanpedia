0d6f6519fa6dd24a24d09962eeb6167c675ca0ae
paris-liam <lparis@fordham.edu>
2018-05-19 12:07:46 -0400

    netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,7 @@ module.exports = {
     'gatsby-transformer-remark',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-netlify',
     'gatsby-plugin-netlify-cms',
     'gatsby-image',
   ],
