c51b96325fbaeccb71ff1eeec8d5d9b891ef3bfe
cream301 <39139542+cream301@users.noreply.github.com>
2018-11-30 10:24:19 -0500

    creating sitemap

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,34 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+import fs from 'fs-extra-promise'
+import sm from 'sitemap' // npm install --save sitemap
 
- // You can delete this file if you're not using it
+function pagesToSitemap(pages) {
+  const urls = pages.map((p) => {
+    if (p.path !== undefined) {
+      return {
+        url: p.path,
+        changefreq: 'daily',
+        priority: 0.7
+      }
+    }
+  })
+  // remove undefined (template pages)
+  return urls.filter(u => u !== undefined)
+}
+
+function generateSiteMap(pages) {
+  const sitemap = sm.createSitemap({
+    hostname: 'https://ryanpedia.netlify.com',
+    cacheTime: '60000',
+    urls: pagesToSitemap(pages),
+  })
+  console.log('Generating sitemap.xml')
+  fs.writeFileSync(
+    `${__dirname}/public/sitemap.xml`,
+    sitemap.toString()
+  )
+}
+
+export function postBuild(pages, callback) {
+  generateSiteMap(pages)
+  callback()
+}
