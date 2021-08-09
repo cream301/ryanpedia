d214053d8c43fd39b3148c51954bb7f6463e98ac
cream301 <39139542+cream301@users.noreply.github.com>
2021-08-09 14:00:12 -0700

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -23,7 +23,7 @@ const Layout = ({ children, data }) => (
     <Helmet
       title="Ryan Creamer"
       meta={[
-    { name: 'description', content: "Ryan Creamer is a comedian. He currently works as a headline contributor at Clickhole. He's a former CollegeHumor castmember, Upright Citizens Brigade Theater performer, and Pornhub pornstar. He's most well known for being a photo on the 'Movie Theater' Wikipedia page under the 'Pricing and Admission' section as 'A Theatre-Goer Enjoys a Show'." },
+    { name: 'description', content: "Ryan Creamer currently works as a headline contributor at Clickhole. He's a former CollegeHumor castmember, Upright Citizens Brigade Theater performer, and Pornhub pornstar. He's most well known for being a photo on the 'Movie Theater' Wikipedia page under the 'Pricing and Admission' section as 'A Theatre-Goer Enjoys a Show'." },
     { name: 'keywords', content: 'Ryan Creamer, pornhub, Ryan, Creamer, comedy,' },
     { name: 'viewport', content: 'width=device-width, initial-scale=.5' },
   {name:"google-site-verification", content:"gmPyHVp4O2asQXGxqe-mczFszN5KN-3OQcnJP4UG4Ns"},
